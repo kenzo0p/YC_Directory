ad66bbdd4753fbc654c56febd7d73b086906d552
Om Bhor <ombhor617@gmail.com>
2025-01-05 23:30:37 +0530

    Forms in next

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
+import SearchForm from "../../components/SearchForm";
 
-export default function Home() {
+export default async function Home({searchParams} : {searchParams :Promise<{query?:string}>})  {
+  const query = (await searchParams).query
+
   return (
     <>
       <section className="pink_container">
@@ -11,6 +14,7 @@ export default function Home() {
           Submit Ideas ,Vite on Pitches, and Get Noticed in Virtual
           Competitions.
         </p>
+        <SearchForm query={query}/>
       </section>
     </>
   );
